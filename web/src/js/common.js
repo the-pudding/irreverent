15d132e2ae024a9860956bea1b2a670bc509ff28
Amber Thomas <ProQuestionAsker@gmail.com>
2020-05-04 17:20:12 -0700

    setting up the repo

---
+++ web/src/js/common.js
@@ -0,0 +1,54 @@
+import loadData from './load-data';
+
+// selections
+const $container = d3.select('.common');
+const $figure = $container.select('.figure__inner');
+
+let data = [];
+
+function resize() {}
+
+function generateWordGroups() {
+  const $group = $figure
+    .selectAll('.group')
+    .data(data)
+    .join((enter) =>
+      enter.append('div').attr('class', (d) => `group group--${d.word}`)
+    );
+
+  $group
+    .selectAll('.word')
+    .data((d) => {
+      console.log({ d });
+      return d.n;
+    })
+    .join((enter) => {
+      const div = enter.append('div').attr('class', 'word');
+
+      div
+        .append('p')
+        .attr('class', 'word__text')
+        .text((d) => d);
+    });
+}
+
+function cleanData(res) {
+  const clean = res.map((d) => ({
+    ...d,
+    n: d3.range(0, +d.n / 2).map(() => d.word),
+  }));
+
+  return clean;
+}
+
+function init() {
+  loadData('common.csv')
+    .then((result) => {
+      data = cleanData(result);
+      console.log({ data });
+      generateWordGroups();
+    })
+    .catch(console.error);
+}
+
+export default { init, resize };
